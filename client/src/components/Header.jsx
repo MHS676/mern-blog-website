73290bb94ec4c1aa182a71ec30043c2942b8d583
Hasan Talukder <mahadihasansiam2@gmail.com>
2024-07-13 01:02:36 +0600

    create and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -24,12 +24,12 @@ const Header = () => {
         <AiOutlineSearch/>
       </Button>
       <div className="flex gap-2 md:order-2">
-        <Button className=" w-1/2 h-10 hidden sm:inline" color='gray' pill>
+        <Button className=" w-12 h-10 hidden sm:inline" color='gray' pill>
           <FaMoon/>
           
         </Button>
         <Link to='/sign-in'>
-              <Button gradientDuoTone='purpleToBlue'>
+              <Button gradientDuoTone='purpleToBlue' outline>
                 Sign in
               </Button>
           </Link>
