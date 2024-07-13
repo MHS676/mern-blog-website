67f9e3adc70ba78335b596828524ff0313b45a6c
Hasan Talukder <mahadihasansiam2@gmail.com>
2024-07-13 12:41:21 +0600

    create test api routes

--- api/index.js
+++ api/index.js
@@ -1,17 +1,22 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import 'dotenv/config'
+import dotenv from 'dotenv';   
+import userRoutes from './routes/user.route.js';
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mern-blog.tl1lwtp.mongodb.net/?retryWrites=true&w=majority&appName=mern-blog`).then(() => {
-    console.log('mongodb is connected')
-})
-.catch((err) => {
-    console.log(err)
-})
+dotenv.config();  
 
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mern-blog.tl1lwtp.mongodb.net/?retryWrites=true&w=majority&appName=mern-blog`)
+    .then(() => {
+        console.log('mongodb is connected');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 const app = express();
 
+app.use('/api/user', userRoutes);
+
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
+    console.log('Server is running on port 3000');
 });
