397912fa67890a11b10b45af75abf62ea3f73a86
Hasan Talukder <mahadihasansiam2@gmail.com>
2024-07-13 11:57:39 +0600

    connect to the database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config'
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mern-blog.tl1lwtp.mongodb.net/?retryWrites=true&w=majority&appName=mern-blog`).then(() => {
+    console.log('mongodb is connected')
+})
+.catch((err) => {
+    console.log(err)
+})
+
 
 const app = express();
 
-app.listen(300, () => {
+app.listen(3000, () => {
     console.log('Server is running on port 3000')
 });
