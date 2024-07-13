de574517e9a8643019dd214df9c08b5bc8f6a772
Hasan Talukder <mahadihasansiam2@gmail.com>
2024-07-13 17:18:20 +0600

    Add functionality to the sign up page

--- api/index.js
+++ api/index.js
@@ -1,12 +1,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';   
+import dotenv from 'dotenv';
+import cors from 'cors';
 import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js'
+import authRoutes from './routes/auth.route.js';
 
-
-
-dotenv.config();  
+dotenv.config();
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@mern-blog.tl1lwtp.mongodb.net/?retryWrites=true&w=majority&appName=mern-blog`)
     .then(() => {
@@ -18,6 +17,7 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@me
 
 const app = express();
 
+app.use(cors());  // Enable CORS for all routes
 app.use(express.json());
 
 app.use('/api/user', userRoutes);
@@ -28,11 +28,11 @@ app.listen(3000, () => {
 });
 
 app.use((err, req, res, next) => {
-  const statusCode = err.statusCode || 500;
-  const message = err.message || 'Internal Server Error';
-  res.status(statusCode).json({
-    success: false,
-    statusCode,
-    message,
-  });
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
 });
